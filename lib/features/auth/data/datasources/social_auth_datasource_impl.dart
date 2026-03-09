227297a85581e512ec56d5e4ced0b2617629b413
Usama Elgendy <oelgindy@moj.gov.sa>
2026-03-10 00:01:14 +0200

    initialize google sign-in before authentication process

--- lib/features/auth/data/datasources/social_auth_datasource_impl.dart
+++ lib/features/auth/data/datasources/social_auth_datasource_impl.dart
@@ -1,18 +1,33 @@
 import 'package:auth_flow_app/core/error/exceptions.dart';
-import 'package:auth_flow_app/features/auth/data/models/user_model.dart';
-import 'package:auth_flow_app/features/auth/data/datasources/social_auth_datasource.dart';
 import 'package:auth_flow_app/core/network/supabase/auth_client.dart';
+import 'package:auth_flow_app/features/auth/data/datasources/social_auth_datasource.dart';
+import 'package:auth_flow_app/features/auth/data/models/user_model.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
 class SocialAuthDataSourceImpl implements SocialAuthDataSource {
   final AuthClient _authClient;
 
+  final GoogleSignIn _googleSignIn = GoogleSignIn.instance;
+
+  bool _isInitialized = false;
+
   SocialAuthDataSourceImpl(this._authClient);
+
+  Future ensureInitialized() async {
+    if (_isInitialized) return;
+    await _googleSignIn.initialize(
+      serverClientId: '227132170531-c9sllos73r6754f8lfbk90osdh9og12p.apps.googleusercontent.com',
+    );
+    _isInitialized = true;
+  }
 
   @override
   Future<UserModel> signInWithGoogle() async {
     try {
-      // TODO: Implement signInWithGoogle
-      throw UnimplementedError('signInWithGoogle not implemented yet');
+      await ensureInitialized();
+      final GoogleSignInAccount googleUser = await _googleSignIn.authenticate();
+
+      return UserModel(id: googleUser.id, email: googleUser.email, isEmailVerified: true, createdAt: DateTime.now());
     } on AuthException {
       rethrow;
     } catch (e) {
